0f56d59d9c76053b20626522f99fa68df3cae87a
sunghyun <sunghyun1038@naver.com>
2017-08-02 15:50:47 +0900

    간헐적으로 캡쳐 혹은 매크로 기능 사용할 때 캡쳐화면이 중복으로 노출되는 이슈 singletop flag를 추가하여 수정

--- app/src/main/java/model/MacroScript.java
+++ app/src/main/java/model/MacroScript.java
@@ -3,6 +3,9 @@ package model;
 /**
  * Created by NAVER on 2017-08-02.
  * Macro Script SingleTon
+ * 사용법
+ * 1. cmd -> /android-sdks/platform-tools에서 adb.exe shell 입력
+ * 2. monkey -p com.campmobile.snow -v -v -f /storage/sdcard0/Android/data/a.txt 5
  */
 
 public class MacroScript {
